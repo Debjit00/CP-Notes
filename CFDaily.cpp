7d526ee6f4a0a905eb4428c465e1bdc803139512
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2023-09-15 10:45:05 +0530

    Create CFDaily.cpp

---
+++ CFDaily.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define loop(n) for(int i=0; i<n; i++)
+#define rloop(n) for(int i=n-1; i>=0; i--)
+
+int solve() {
+    int n, pos=0, neg=0, ans=0;
+    cin >> n;
+    loop(n) {
+        cin >> ans;
+        if (ans==1) pos++;
+        else neg++;
+    }
+
+    ans=neg%2;
+    if (neg-neg%2>n/2) ans += neg-neg%2-n/2+n/2%2;
+
+    return ans;
+}
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) cout << solve() << endl;
+    return 0;
+}
