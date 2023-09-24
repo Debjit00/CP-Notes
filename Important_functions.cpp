574d0783c1d73247b5b87a813ec2ddb9fe3caa8c
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2023-09-24 11:08:44 +0530

    Create Important_functions.cpp

---
+++ Important_functions.cpp
@@ -0,0 +1,22 @@
+string stringDiffs(string num1, string num2) {
+    int len1 = num1.length(), len2 = num2.length();
+    if (len1 < len2 || (len1 == len2 && num1 < num2))
+        swap(num1, num2), swap(len1, len2);
+
+    string result = "";
+    int carry = 0;
+    for (int i=0; i<len1; i++) {
+        int digit1 = num1[len1 - 1 - i] - '0';
+        int digit2 = i < len2 ? num2[len2 - 1 - i] - '0' : 0;
+        int diff = digit1 - digit2 - carry;
+        if (diff < 0) diff += 10, carry = 1;
+        else carry = 0;
+
+        result = to_string(diff) + result;
+    }
+
+    while (result.length() > 1 && result[0] == '0')
+        result.erase(0, 1);
+
+    return result;
+}
