f9d767d08587494b9c663290cfaf74d632abf744
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2023-11-08 14:07:21 +0530

    Update 00_Algorithms.cpp

--- 00_Algorithms.cpp
+++ 00_Algorithms.cpp
@@ -18,7 +18,7 @@ int totalSubsequences(int n, int k, vector<int> &arr) {
     for(int i=0; i<n; i++) dp[i][0] = 1;
     if (arr[0] <= k) dp[0][arr[0]] += 1;
     for (int i=1; i<n; i++) {
-        for (int target=1; target <= k; target++) {
+        for (int target=0; target <= k; target++) {
             if (target >= arr[i]) 
                 dp[i][target] = (dp[i-1][target] + dp[i-1][target-arr[i]]) % mod;
             else dp[i][target] = dp[i-1][target];
