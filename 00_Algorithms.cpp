704e1918474b6d4cc89d79ff1555e3eab9acfac0
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2023-11-08 13:17:17 +0530

    Update 00_Algorithms.cpp

--- 00_Algorithms.cpp
+++ 00_Algorithms.cpp
@@ -5,16 +5,16 @@ bool isPossible(int n, int k, vector<int> &arr) {
 	if (k >= arr[0]) dp[0][arr[0]] = 1;
 	for (int i=1; i<n; i++) {
 	    for (int target=1; target <= k; target++) {
-	        if (target >= arr[i]) 
-	            dp[i][target] = dp[i-1][target] | dp[i-1][target-arr[i]];
-	        else dp[i][target] = dp[i-1][target];
+		if (target >= arr[i]) 
+		    dp[i][target] = dp[i-1][target] | dp[i-1][target-arr[i]];
+		else dp[i][target] = dp[i-1][target];
 	    }
 	}
 	return dp[n-1][k];
 }
 
 int totalSubsequences(int n, int k, vector<int> &arr) {
-	vector<vector<int>> dp(n, vector<int> (k+1));
+vector<vector<int>> dp(n, vector<int> (k+1));
     for(int i=0; i<n; i++) dp[i][0] = 1;
     if (arr[0] <= k) dp[0][arr[0]] += 1;
     for (int i=1; i<n; i++) {
