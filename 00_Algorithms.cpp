7214b985335bdf5cdc2dc6fb6df582673007850a
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2023-11-08 13:16:32 +0530

    Update 00_Algorithms.cpp

--- 00_Algorithms.cpp
+++ 00_Algorithms.cpp
@@ -1,3 +1,32 @@
+// DP Subset Sum
+bool isPossible(int n, int k, vector<int> &arr) {
+	vector<vector<bool>> dp(n, vector<bool> (k+1));
+	for(int i=0; i<n; i++) dp[i][0] = 1;
+	if (k >= arr[0]) dp[0][arr[0]] = 1;
+	for (int i=1; i<n; i++) {
+	    for (int target=1; target <= k; target++) {
+	        if (target >= arr[i]) 
+	            dp[i][target] = dp[i-1][target] | dp[i-1][target-arr[i]];
+	        else dp[i][target] = dp[i-1][target];
+	    }
+	}
+	return dp[n-1][k];
+}
+
+int totalSubsequences(int n, int k, vector<int> &arr) {
+	vector<vector<int>> dp(n, vector<int> (k+1));
+    for(int i=0; i<n; i++) dp[i][0] = 1;
+    if (arr[0] <= k) dp[0][arr[0]] += 1;
+    for (int i=1; i<n; i++) {
+        for (int target=1; target <= k; target++) {
+            if (target >= arr[i]) 
+                dp[i][target] = (dp[i-1][target] + dp[i-1][target-arr[i]]) % mod;
+            else dp[i][target] = dp[i-1][target];
+        }
+    }
+    return dp[n-1][k];
+}
+
 // DFS
 void dfs(int node, vector<vector<int>> &adjLs, vector<int> &vis) {
 	vis[node] = 1;
@@ -17,18 +46,6 @@ for (int i=0; i<V; i++) {
 			adjLs[j].push_back(i);
 		}
 	}
-}
-
-// DP Subset Sum
-vector<vector<bool>> dp(n, vector<bool> (k+1));
-for(int i=0; i<n; i++) dp[i][0] = 1;
-if (k >= arr[0]) dp[0][arr[0]] = 1;
-for (int i=1; i<n; i++) {
-    for (int target=1; target <= k; target++) {
-        if (target >= arr[i]) 
-            dp[i][target] = dp[i-1][target] | dp[i-1][target-arr[i]];
-        else dp[i][target] = dp[i-1][target];
-    }
 }
 
 // SEGMENT TREE
