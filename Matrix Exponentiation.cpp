4f6eb8399d3a940840b5a5e151c60669149692ff
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2023-11-22 23:42:48 +0530

    Create Matrix Exponentiation.cpp

---
+++ Matrix Exponentiation.cpp
@@ -0,0 +1,31 @@
+vector<vector<ll>> multiply(vector<vector<ll>> &A, vector<vector<ll>> &B){
+    vector<vector<ll> >result(A.size(), vector<ll>(B[0].size(),0));
+    if(A[0].size()==B.size()){
+        const int N=A[0].size();
+        for(int i=0; i<A.size(); i++) {
+            for(int j=0; j<B[0].size(); j++) {
+                for(int k=0; k<A[0].size(); k++) {
+                    result[i][j]+=A[i][k]*B[k][j];
+                    result[i][j] %= mod;
+                }
+            }
+        }
+    }
+    return result;
+}
+
+vector<vector<ll>> pwr(vector<vector<ll>> &A, int N) {
+    int n = A.size();
+    vector<vector<ll>> res (n , vector<ll> (n));
+    for(int i = 0;i < n;i++) {
+        for(int j = 0;j < n; j++) {
+            if(i == j) res[i][j] = 1;
+        }
+    }
+    while(N) {
+        if(N % 2) res = multiply(res, A);
+        A = multiply(A, A);
+        N /= 2;
+    }
+    return res;
+}
