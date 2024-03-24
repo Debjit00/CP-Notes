5999520c7df736759d0e99cdc424102b930020f2
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2024-03-24 14:44:20 +0530

    Update GRAPHS.cpp

--- GRAPHS.cpp
+++ GRAPHS.cpp
@@ -1,3 +1,46 @@
+// Disjoint Set Union(DSU):
+struct UnionFind {
+    int n, set_size, *parent, *rank;
+    UnionFind(){}
+    UnionFind(int a) {
+        n = set_size = a;
+        parent = new int[n+1];
+        rank = new int[n+1];
+        for(int i=1; i<=n; i++) 
+            parent[i]=i, rank[i]=1;
+    }
+    int find(int x) {
+        if(x != parent[x]) return parent[x] = find(parent[x]);
+        else return x;
+    }
+    void merge(int x, int y) {
+        int xroot = find(x), yroot = find(y);
+        if (xroot != yroot) {
+            if(rank[xroot] >= rank[yroot]) {
+                parent[yroot] = xroot;
+                rank[xroot] += rank[yroot];
+            }
+            else {
+                parent[xroot] = yroot;
+                rank[yroot] += rank[xroot];
+            }
+            set_size -= 1;
+        }
+    }
+    void reset() {
+        set_size=n;for(int i=1;i<=n;i++)parent[i]=i,rank[i]=1;}
+    int size() {return set_size;}
+};
+
+// Floyd Warshall's algorithms:
+void floydWarshall(vvll &dist, int n) {
+    for (int k=0; k < n; k++)
+        for (int i=0; i < n; i++)
+            for (int j=0; j < n; j++)
+                if (dist[i][j] > (dist[i][k] + dist[k][j]) && (dist[k][j] < 2e9 && dist[i][k] != INF))
+                    dist[i][j] = dist[i][k] + dist[k][j];
+}
+
 // ADJACENCY GRID
 void dfs(int r, int c, vector<vector<int>> &grid, vector<vector<int>>  &vis) {
     int n=grid.size();
@@ -44,26 +87,3 @@ void bfs(int row, int col, vector<vector<int>> &grid, vector<vector<int>> &vis) 
     
 }
 //*************************************************************************************************************************************************************************
-
-// ADJACENCY LIST
-void bfs(int node, vector<int> adj[], vector<int> &vis) {
-    queue<int> q;
-    q.push(node);
-    vis[node] = 1;
-    
-    while(!q.empty()) {
-        node = q.front();
-        q.pop();
-        
-        for(int it: adj[node]) {
-            if(!vis[it]) {
-                vis[it] = 1;
-                q.push(it);
-            }
-        }
-    }
-    
-}
-
-void dfs(
-
