8ece0e2903ebfcafef2f79b2188da7ba5080657e
Debjit Saha <118826134+Debjit00@users.noreply.github.com>
2024-05-28 20:02:55 +0530

    Update 01_PrimeMethods.cpp

--- 01_PrimeMethods.cpp
+++ 01_PrimeMethods.cpp
@@ -1,5 +1,5 @@
 Prime Methods:
-bool isPrime(ll n) {
+bool isPrime(int n) {
     if (n==2 || n==3) return true;
     if (n<2 || n%2==0 || n%3==0) return false;
     int factor=3;
@@ -10,7 +10,7 @@ bool isPrime(ll n) {
     return true;
 }
 
-ll len=1e5;
+int len=1e5;
 vector<bool> prime(len+1, 1);
 void build() {
     prime[0]=0, prime[1]=0;
@@ -20,4 +20,23 @@ void build() {
             if (prime[i]) prime[i]=false;
         }
     }
+}
+
+// Prime factors
+int len = 1e5;
+vector<int> spf(len + 1, 1);
+void sieve() {
+    spf[0] = 0;
+    for (int i = 2; i <= len; i++)
+    if (spf[i] == 1)
+    for (int j = i; j <= len; j += i)
+    if (spf[j]== 1) spf[j] = i;
+}
+vector<int> factors(int x) {
+    vector<int> res;
+    while (x != 1) {
+        res.push_back(spf[x]);
+        x = x / spf[x];
+    }
+    return res;
 }
